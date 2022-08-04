757de81e18e51d0f18a55c6c5a967fa417c4f122
machao07 <machao_07@163.com>
2022-08-04 17:51:54 +0800

    taro-ui init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// 具体配置可以参考 https://prettier.io/docs/en/options.html
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    semi: false, // 未尾逗号
+    singleQuote: true, // 单引号
+    bracketSpacing: true,
+    trailingComma: 'none', // 未尾分号
+    proseWrap: 'never',
+    htmlWhitespaceSensitivity: 'strict'
+};
