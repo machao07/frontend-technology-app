5b2ddc96317001ca95d7472ab2ea7b6182343f63
machao07 <machao_07@163.com>
2022-08-07 13:05:05 +0800

    add statistics module

--- src/pages/statistics/index.config.ts
+++ src/pages/statistics/index.config.ts
@@ -1,3 +1,6 @@
 export default {
-  navigationBarTitleText: '前端进阶技术栈'
+    navigationBarTitleText: '前端进阶技术栈',
+    usingComponents: {
+        "ec-canvas": "../../ec-canvas/ec-canvas"
+    }
 }
--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,14 +1,174 @@
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import { Component } from 'react'
 import TabBar from '../../components/tarBar'
 import { AtTabs, AtTabsPane } from 'taro-ui'
+// @ts-ignore
+import * as echarts from "../../ec-canvas/echarts";
 import './index.scss'
 
-export default class Statistics extends Component<any, any> {
-    constructor(props) {
+function getLineChart(canvas, width, height, dpr) {
+    const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // 像素
+    });
+    canvas.setChart(chart);
+
+    var option = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['Vue', 'React', 'Angular']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            data: ['Jul 3', 'Jul 10', 'Jul 17', 'Jul 24', 'Jul 31',]
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                rotate: 30
+            }
+        },
+        series: [
+            {
+                name: 'Vue',
+                type: 'line',
+                data: [3368044, 3345639, 2861325, 3314171, 3171489]
+            },
+            {
+                name: 'React',
+                type: 'line',
+                data: [14523799, 15420684, 13444250, 15781657, 15102270]
+            },
+            {
+                name: 'Angular',
+                type: 'line',
+                data: [509874, 522777, 466688, 515245, 516269]
+            },
+        ]
+    }
+    chart.setOption(option);
+    return chart;
+}
+
+function getPieChart(canvas, width, height, dpr) {
+    const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // 像素
+    });
+    canvas.setChart(chart);
+
+    var option = {
+        tooltip: {
+            show: true
+        },
+        legend: {
+            top: 'bottom'
+        },
+        series: [
+            {
+                name: 'Nightingale Chart',
+                type: 'pie',
+                radius: [35, 110],
+                center: ['50%', '50%'],
+                roseType: 'area',
+                itemStyle: {
+                    borderRadius: 8
+                },
+                data: [
+                    { value: 56.7, name: 'Vue' },
+                    { value: 36.4, name: 'Angular' },
+                    { value: 72.5, name: 'React' }
+                ]
+            }
+        ]
+    }
+    chart.setOption(option);
+    return chart;
+}
+
+function getBarChart(canvas, width, height, dpr) {
+    const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // 像素
+    });
+    canvas.setChart(chart);
+
+    var option = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['Express', 'Koa', 'Mysql']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            data: ['Jul 3', 'Jul 10', 'Jul 17', 'Jul 24', 'Jul 31',]
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                rotate: 30
+            }
+        },
+        series: [
+            {
+                name: 'Express',
+                type: 'bar',
+                data: [1164582, 23814867, 20821804, 24959497, 25038235]
+            },
+            {
+                name: 'Koa',
+                type: 'bar',
+                data: [1209957, 1303051, 1156994, 1298462, 1370593]
+            },
+            {
+                name: 'Mysql',
+                type: 'bar',
+                data: [895478, 852151, 722522, 859523, 854581]
+            },
+        ]
+    }
+    chart.setOption(option);
+    return chart;
+}
+
+interface States {
+    current: number
+    ecLine: any
+    ecBar: any
+    ecPie: any
+}
+export default class Statistics extends Component<any, States> {
+    constructor(props: any) {
         super(props)
         this.state = {
             current: 0,
+            ecLine: {
+                onInit: getLineChart
+            },
+            ecPie: {
+                onInit: getPieChart
+            },
+            ecBar: {
+                onInit: getBarChart
+            }
         }
     }
 
@@ -19,24 +179,36 @@ export default class Statistics extends Component<any, any> {
     }
 
     render() {
+        const {ecLine, ecBar, ecPie } = this.state
         return (
             <View className="statistics">
                 <AtTabs
+                    className="tabBox"
                     current={this.state.current}
                     tabList={[
                         { title: '前端框架' },
-                        { title: '标签页2' }
+                        { title: '生态圈' }
                     ]}
                     onClick={this.handleClick.bind(this)}>
                     <View className="main">
                         <AtTabsPane current={this.state.current} index={0}>
-                            <View style='font-size:18px;text-align:center;height:100px;'>标签页一的内容</View>
+                            <Text className="eChartTitle">2022年NPM下载量</Text>
+                            <View className="canvas bar" style={{ margin: '30px 0' }}>
+                                <ec-canvas id="mychart-dom-bar" canvas-id="mychart-bar" ec={ecLine}></ec-canvas>
+                            </View>
+
+                            <Text className="eChartTitle">框架权重比较（%）</Text>
+                            <View className="canvas pie">
+                                <ec-canvas id="mychart-dom-pie" canvas-id="mychart-pie" ec={ecPie}></ec-canvas>
+                            </View>
                         </AtTabsPane>
                         <AtTabsPane current={this.state.current} index={1}>
-                            <View style='font-size:18px;text-align:center;height:100px;'>标签页二的内容</View>
+                            <Text className="eChartTitle">前端相关技术栈</Text>
+                            <View className="canvas bar" style={{ margin: '30px 0' }}>
+                                <ec-canvas id="mychart-dom-bar" canvas-id="mychart-bar" ec={ecBar}></ec-canvas>
+                            </View>
                         </AtTabsPane>
                     </View>
-
                 </AtTabs>
                 <TabBar currentIndex={1} />
             </View>
