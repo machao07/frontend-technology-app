f1bc8b1839ecc807b7a4a1a65c8ac50317b492d9
machao07 <machao_07@163.com>
2022-08-08 11:22:57 +0800

    add ScrollView

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, ScrollView } from '@tarojs/components'
 import { Component } from 'react'
 import TabBar from '../../components/tarBar'
 import { AtTabs, AtTabsPane } from 'taro-ui'
 // @ts-ignore
 import * as echarts from "../../ec-canvas/echarts";
 import './index.scss'
+import Taro from '@tarojs/taro';
 
 function getLineChart(canvas, width, height, dpr) {
     const chart = echarts.init(canvas, null, {
@@ -179,11 +180,15 @@ export default class Statistics extends Component<any, States> {
     }
 
     render() {
-        const {ecLine, ecBar, ecPie } = this.state
+        const { ecLine, ecBar, ecPie } = this.state
+        const { windowHeight } = Taro.getSystemInfoSync()
+        const scrollHeight = (windowHeight * windowHeight / 750 - 140)
+        const scrollStyle = {
+            height: scrollHeight + 'px',
+        }
         return (
             <View className="statistics">
                 <AtTabs
-                    className="tabBox"
                     current={this.state.current}
                     tabList={[
                         { title: '前端框架' },
@@ -192,16 +197,26 @@ export default class Statistics extends Component<any, States> {
                     onClick={this.handleClick.bind(this)}>
                     <View className="main">
                         <AtTabsPane current={this.state.current} index={0}>
-                            <Text className="eChartTitle">前端框架NPM下载量/月（2022）</Text>
-                            <View className="canvas bar" style={{ margin: '30px 0' }}>
-                                <ec-canvas id="mychart-dom-bar" canvas-id="mychart-bar" ec={ecLine}></ec-canvas>
-                            </View>
+                            <ScrollView
+                                scrollY
+                                enhanced={true}
+                                show-scrollbar={false}
+                                scrollWithAnimation={true}
+                                enable-back-to-top={true}
+                                style={scrollStyle}
+                            >
+                                <Text className="eChartTitle">前端框架NPM下载量/月（2022）</Text>
+                                <View className="canvas bar" style={{ margin: '30px 0' }}>
+                                    <ec-canvas id="mychart-dom-bar" canvas-id="mychart-bar" ec={ecLine}></ec-canvas>
+                                </View>
 
-                            <Text className="eChartTitle">框架权重比较（%）</Text>
-                            <View className="canvas pie">
-                                <ec-canvas id="mychart-dom-pie" canvas-id="mychart-pie" ec={ecPie}></ec-canvas>
-                            </View>
+                                <Text className="eChartTitle">框架权重比较（%）</Text>
+                                <View className="canvas pie">
+                                    <ec-canvas id="mychart-dom-pie" canvas-id="mychart-pie" ec={ecPie}></ec-canvas>
+                                </View>
+                            </ScrollView>
                         </AtTabsPane>
+
                         <AtTabsPane current={this.state.current} index={1}>
                             <Text className="eChartTitle">服务端框架NPM下载量/月（2022）</Text>
                             <View className="canvas bar" style={{ margin: '30px 0' }}>
