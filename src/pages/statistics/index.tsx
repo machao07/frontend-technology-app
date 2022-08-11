32a01639f2265478189a46cc0bff4100383bc734
machao07 <machao_07@163.com>
2022-08-11 10:07:42 +0800

    code优化

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -6,6 +6,7 @@ import { AtTabs, AtTabsPane } from 'taro-ui'
 import * as echarts from "../../ec-canvas/echarts";
 import './index.scss'
 import Taro from '@tarojs/taro';
+import { lineOptions, barOptions, pieOptions } from './options'
 
 function getLineChart(canvas, width, height, dpr) {
     const chart = echarts.init(canvas, null, {
@@ -15,47 +16,7 @@ function getLineChart(canvas, width, height, dpr) {
     });
     canvas.setChart(chart);
 
-    var option = {
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data: ['Vue', 'React', 'Angular']
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            type: 'category',
-            data: ['Jul 3', 'Jul 10', 'Jul 17', 'Jul 24', 'Jul 31',]
-        },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                rotate: 30
-            }
-        },
-        series: [
-            {
-                name: 'Vue',
-                type: 'line',
-                data: [3368044, 3345639, 2861325, 3314171, 3171489]
-            },
-            {
-                name: 'React',
-                type: 'line',
-                data: [14523799, 15420684, 13444250, 15781657, 15102270]
-            },
-            {
-                name: 'Angular',
-                type: 'line',
-                data: [509874, 522777, 466688, 515245, 516269]
-            },
-        ]
-    }
+    var option = lineOptions;
     chart.setOption(option);
     return chart;
 }
@@ -68,31 +29,7 @@ function getPieChart(canvas, width, height, dpr) {
     });
     canvas.setChart(chart);
 
-    var option = {
-        tooltip: {
-            show: true
-        },
-        legend: {
-            top: 'bottom'
-        },
-        series: [
-            {
-                name: 'Nightingale Chart',
-                type: 'pie',
-                radius: [35, 110],
-                center: ['50%', '50%'],
-                roseType: 'area',
-                itemStyle: {
-                    borderRadius: 8
-                },
-                data: [
-                    { value: 56.7, name: 'Vue' },
-                    { value: 36.4, name: 'Angular' },
-                    { value: 72.5, name: 'React' }
-                ]
-            }
-        ]
-    }
+    var option = pieOptions;
     chart.setOption(option);
     return chart;
 }
@@ -105,47 +42,7 @@ function getBarChart(canvas, width, height, dpr) {
     });
     canvas.setChart(chart);
 
-    var option = {
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data: ['Express', 'Koa', 'Mysql']
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            type: 'category',
-            data: ['Jul 3', 'Jul 10', 'Jul 17', 'Jul 24', 'Jul 31',]
-        },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                rotate: 30
-            }
-        },
-        series: [
-            {
-                name: 'Express',
-                type: 'bar',
-                data: [1164582, 23814867, 20821804, 24959497, 25038235]
-            },
-            {
-                name: 'Koa',
-                type: 'bar',
-                data: [1209957, 1303051, 1156994, 1298462, 1370593]
-            },
-            {
-                name: 'Mysql',
-                type: 'bar',
-                data: [895478, 852151, 722522, 859523, 854581]
-            },
-        ]
-    }
+    var option = barOptions;
     chart.setOption(option);
     return chart;
 }
@@ -207,7 +104,7 @@ export default class Statistics extends Component<any, States> {
                             >
                                 <Text className="eChartTitle">前端框架NPM下载量/月（2022）</Text>
                                 <View className="canvas bar" style={{ margin: '30px 0' }}>
-                                    <ec-canvas id="mychart-dom-bar" canvas-id="mychart-bar" ec={ecLine}></ec-canvas>
+                                    <ec-canvas id="mychart-dom-line" canvas-id="mychart-line" ec={ecLine}></ec-canvas>
                                 </View>
 
                                 <Text className="eChartTitle">框架权重比较（%）</Text>
