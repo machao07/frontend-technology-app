da4126e9fc0a45e129ad8ce282cfff17ccee5881
machao07 <machao_07@163.com>
2023-12-05 23:59:31 +0800

    压缩js

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,27 @@
 module.exports = {
-  env: {
-    NODE_ENV: '"development"'
-  },
-  defineConstants: {
-  },
-  mini: {},
-  h5: {}
+    env: {
+        NODE_ENV: '"development"'
+    },
+    defineConstants: {
+    },
+    mini: {
+        webpackChain: (chain, webpack) => {
+            chain.merge({
+                plugin: {
+                    install: {
+                        plugin: require('terser-webpack-plugin'),
+                        args: [{
+                            terserOptions: {
+                                compress: true, // 默认使用terser压缩
+                                // mangle: false,
+                                keep_classnames: true, // 不改变class名称
+                                keep_fnames: true // 不改变函数名称
+                            }
+                        }]
+                    }
+                }
+            })
+        }
+    },
+    h5: {}
 }
